1bcd4ad309e402a824d29bd7af902c1c2c90b7ce
Mani <mkanamarlapudi@tmhcc.com>
2021-12-27 00:38:46 +0530

    added skeliton structure

---
+++ lib/index.d.ts
@@ -0,0 +1,10 @@
+export declare const Greeter: (name: string) => string;
+export declare class IsValid {
+    static Mail: (val: string) => boolean;
+    static Email: (val: string) => boolean;
+    static IpAddress: (val: string) => boolean;
+    static AadharNumber: (val: string) => boolean;
+    static PanCard: (val: string) => boolean;
+    static PinCode: (val: string) => boolean;
+    static Phone: (val: string) => boolean;
+}
---
+++ src/index.ts
@@ -0,0 +1,31 @@
+export const Greeter = (name: string) => `Hello ${name}`; 
+
+export class IsValid {
+    
+    public static Mail = (val:string): boolean => {
+        return true        
+    }
+    public static Email = (val:string): boolean => {
+        return true        
+    }
+
+    public static IpAddress = (val:string): boolean => {
+        return true        
+    }
+
+    public static AadharNumber = (val:string): boolean => {
+        return true        
+    }
+
+    public static PanCard = (val:string): boolean => {
+        return true        
+    }
+
+    public static PinCode = (val:string): boolean => {
+        return true        
+    }
+
+    public static Phone = (val:string): boolean => {
+        return true        
+    }
+}
